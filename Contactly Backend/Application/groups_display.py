8af77d63ce6d0e7c8d3a9ac16ab8bf991a38c1c8
Harshit Rathore <105155118+harshitrathor3@users.noreply.github.com>
2024-08-11 22:31:28 +0530

    Update groups_display.py

--- Contactly Backend/Application/groups_display.py
+++ Contactly Backend/Application/groups_display.py
@@ -1,9 +1,9 @@
 from mysql import connector
 # from ..constants import host, database, user, password
-host = 'bkq2iacrckyswhmkbklh-mysql.services.clever-cloud.com'
-database = 'bkq2iacrckyswhmkbklh'
-user = 'uotwmxtea0c3wyvn'
-password = 'uEm0TjBWWEhM0CKPazi5'
+host = ''
+database = ''
+user = ''
+password = ''
 
 try:
     def display_groups(id, token):
